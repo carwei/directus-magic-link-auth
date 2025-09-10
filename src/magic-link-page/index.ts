aa4fffbb1db6fd72a21e950ad7099618945fd1a1
carwei <31320065+carwei@users.noreply.github.com>
2025-09-10 14:02:33 +0200

    sending e-mail works

--- src/magic-link-page/index.ts
+++ src/magic-link-page/index.ts
@@ -2,13 +2,18 @@ import { defineModule } from '@directus/extensions-sdk';
 import ModuleComponent from './module.vue';
 
 export default defineModule({
-	id: 'custom',
-	name: 'Custom',
-	icon: 'box',
+	id: 'magic-link',
+	name: 'Magic Link',
+	icon: 'mail',
 	routes: [
 		{
 			path: '',
 			component: ModuleComponent,
 		},
 	],
+	preRegisterCheck() {
+		// Always allow access - this makes the module public
+		return true;
+	},
+	hidden: true, // Hide from main navigation since it's a public auth page
 });
